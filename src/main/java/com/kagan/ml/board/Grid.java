3d94bb8d001deae2c2682621afa14dd1a9cc6f66
Kaan <kaan@Kaan-MacBook-Pro.local>
2026-03-24 13:51:21 +0000

    feat: add block checker for grid.

--- src/main/java/com/kagan/ml/board/Grid.java
+++ src/main/java/com/kagan/ml/board/Grid.java
@@ -19,6 +19,60 @@ public class Grid {
     public String getCell(int height, int width) { return grid[height][width]; }
     public void setCell(int height, int width, String obj) { grid[height][width] = obj; }
 
+    // Game Control
+    public boolean isVerticalBlock(String marker) {
+        boolean isVerticalBlock = false;
+
+        for (int i = 1; i < width; i += 2) {
+            if (
+                grid[i][1].equals(marker) &&
+                grid[i][3].equals(marker) &&
+                grid[i][5].equals(marker)
+            ) {
+                isVerticalBlock = true;
+            }
+        }
+
+        return isVerticalBlock;
+    }
+
+    public boolean isHorizontalBlock(String marker) {
+        boolean isHorizontalBlock = false;
+
+        for (int i = 1; i < height; i += 2) {
+            if (
+                grid[1][i].equals(marker) &&
+                grid[3][i].equals(marker) &&
+                grid[5][i].equals(marker)
+            ) {
+                isHorizontalBlock = true;
+            }
+        }
+
+        return isHorizontalBlock;
+    }
+
+    public boolean isDiagonalBlock(String marker) {
+        boolean isDiagonalBlock = false;
+
+        if (
+            grid[1][1].equals(marker) &&
+            grid[3][3].equals(marker) &&
+            grid[5][5].equals(marker)
+        ) {
+            isDiagonalBlock = true;
+        }
+
+        if (
+            grid[1][5].equals(marker) &&
+            grid[3][3].equals(marker) &&
+            grid[5][1].equals(marker)
+        ) {
+            isDiagonalBlock = true;
+        }
+        return isDiagonalBlock;
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
--- src/main/java/com/kagan/ml/game/Game.java
+++ src/main/java/com/kagan/ml/game/Game.java
@@ -1,27 +1,34 @@
 package com.kagan.ml.game;
 
 import com.kagan.ml.board.Grid;
+import com.kagan.ml.board.type.Type;
 
 public class Game {
     private final Grid grid;
     private int playerScore;
     private int aiScore;
-    private boolean playerTurn;
+    private boolean playerTurn; // true: Human && false: AI
+    private int verticleParser; 
 
     public Game(Grid grid) {
         this.grid = grid;
         this.playerScore = 0;
         this.aiScore = 0;
-        this.playerTurn = false;
+        this.playerTurn = true;
+        int verticleParser = Type.THREE
     }
 
-    private boolean isBlock(int startY, int stopY, int changeY, int startX, int stopX, int changeX) {
-        boolean isSet = false;
-        for (int i = startY; i < stopY; i += changeY) {
-            for (int j = startX; j < stopX; j += changeX) {
-                grid
-            }
+    private boolean isVerticalBlock() {
+        String marker;
+        if (playerTurn) {
+            marker = Marker.PLAYER_MARKER.getMarker();
+        } else {
+            marker = Marker.AI_MARKER.getMarker();
         }
+
+        boolean isVerticalBlock = false;
+
+        for (int i = 1; i < grid.getHeight() - 6; i += 2)
     }
 
     private boolean isGameFinished() {
