21f1ed5a5c4514741f64e25d93edef896e3b33e1
Kaan <kaan@Kaan-MacBook-Pro.local>
2026-03-24 14:28:51 +0000

    doc: complete documantry of Marker.

--- src/main/java/com/kagan/ml/game/Marker.java
+++ src/main/java/com/kagan/ml/game/Marker.java
@@ -1,7 +1,14 @@
 package com.kagan.ml.game;
 
 public enum Marker {
+    /**
+     * Marker for AI.
+     */
     AI_MARKER("O"),
+
+    /**
+     * Marker for human.
+     */
     PLAYER_MARKER("X");
 
     private final String marker;
@@ -10,6 +17,11 @@ public enum Marker {
         this.marker = marker;
     }
 
+    /**
+     * Human plays with X and AI plays with O.
+     * 
+     * @return type of marker
+     */
     public String getMarker() {
         return this.marker;
     }
