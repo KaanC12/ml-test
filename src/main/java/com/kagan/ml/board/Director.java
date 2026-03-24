77107bbc011561402f1910fadfe1d6b691a6506e
Kaan <kaan@Kaan-MacBook-Pro.local>
2026-03-24 14:39:51 +0000

    doc: documentation of director is completed.

--- src/main/java/com/kagan/ml/board/Director.java
+++ src/main/java/com/kagan/ml/board/Director.java
@@ -1,6 +1,11 @@
 package com.kagan.ml.board;
 
 public class Director {
+    /**
+     * Prepares a board for tic tac toe.
+     * 
+     * @param ttoBoard is a type of board
+     */
     public void makeTTOBoard(Board ttoBoard) {
         ttoBoard.setHorizontals();
         ttoBoard.setVerticals();
