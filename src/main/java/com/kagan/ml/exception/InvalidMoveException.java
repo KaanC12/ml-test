b6d28b250ff78cad382f1a4babb9d9fd35c476f3
kaan <uepef@student.kit.edu>
2026-03-24 23:34:26 +0000

    refactor: add custom exception for invalid moves.

---
+++ src/main/java/com/kagan/ml/exception/InvalidMoveException.java
@@ -0,0 +1,9 @@
+package com.kagan.ml.exception;
+
+public class InvalidMoveException extends Exception {
+
+    public InvalidMoveException(String message) {
+        super(message);
+    }
+    
+}
