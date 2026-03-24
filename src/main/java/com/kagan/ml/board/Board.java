ea1d241849109aaafe3d1700cf4a2d0e71ed7796
Kaan <kaan@Kaan-MacBook-Pro.local>
2026-03-24 14:38:35 +0000

    doc: documentation of board is completed.

--- src/main/java/com/kagan/ml/board/Board.java
+++ src/main/java/com/kagan/ml/board/Board.java
@@ -1,8 +1,19 @@
 package com.kagan.ml.board;
 
 public interface Board {
+    /**
+     * Set spaces on the board.
+     */
     void setSpaces();
+
+    /**
+     * Set "_" for horizontals.
+     */
     void setHorizontals();
+
+    /**
+     * Set "|" for verticals.
+     */
     void setVerticals();
 }
 
