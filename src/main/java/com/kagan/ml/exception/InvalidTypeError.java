03003d7ce9d3d8dcb5efaec8b4bad6c063f388b1
kaan <uepef@student.kit.edu>
2026-03-30 01:29:27 +0100

    doc: add documentation to invalid type.

--- src/main/java/com/kagan/ml/exception/InvalidTypeError.java
+++ src/main/java/com/kagan/ml/exception/InvalidTypeError.java
@@ -1,5 +1,7 @@
 package com.kagan.ml.exception;
-
+/**
+ * Thrown when given type is wrong. 
+ */
 public class InvalidTypeError extends RuntimeException {
     public InvalidTypeError(String message) {
         super(message);
