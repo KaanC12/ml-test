89fbbc647a6434b0aa00f8fa707df1a0e3daf751
kaan <uepef@student.kit.edu>
2026-03-31 03:58:09 +0100

    doc: add class documentation to Board interface.

--- src/main/java/com/kagan/ml/board/Board.java
+++ src/main/java/com/kagan/ml/board/Board.java
@@ -1,5 +1,17 @@
 package com.kagan.ml.board;
 
+/**
+ * Represents what a board can do.
+ * 
+ * A board must perform three functions:
+ * <ul>
+ *  <li>Horizontal edges are represented by {@code _}</li>
+ *  <li>Vertical edges are represented by {@code |}</li>
+ *  <li>Cell spaces are represented by {@code " "}</li>
+ * </ul>
+ * 
+ * @author Kaan Turgut
+ */
 public interface Board {
     /**
      * Set spaces on the board.
