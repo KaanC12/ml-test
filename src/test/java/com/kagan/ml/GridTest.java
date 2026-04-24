fa7a3c5dd237436ab1712a73275fd01233efe245
kaan <uepef@student.kit.edu>
2026-04-24 22:53:29 +0100

    test: add test to modify cell for grid

---
+++ src/test/java/com/kagan/ml/GridTest.java
@@ -0,0 +1,45 @@
+package com.kagan.ml;
+
+import com.kagan.ml.board.Grid;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GridTest {
+    private Grid grid;
+    
+    @BeforeEach
+    void setUp(){
+        grid = new Grid();
+        grid.setHeight(7);
+        grid.setWidth(7);
+        
+        String[][] visualGrid = {
+            {" "," "," "," "," "," "," "},
+            {" "," "," "," "," "," "," "},
+            {" "," "," "," "," "," "," "},
+            {" "," "," "," "," "," "," "},
+            {" "," "," "," "," "," "," "},
+            {" "," "," "," "," "," "," "},
+            {" "," "," "," "," "," "," "}
+        };
+
+        String[][] moveTable = new String[3][3];
+
+        grid.setGrid(visualGrid);
+        grid.setMoveTable(moveTable);
+    }
+
+    @Nested
+    class Cell {
+
+        @Test
+        void setCell_shouldUpdateVisualGrid() {
+            grid.setCell(1, 1, "X");
+
+            assertEquals("X", grid.getCell(1, 1));
+        }
+        
+    }
+}
